cbf88b55d4b084f8e75b288e0502dc80225b0982
Bersek3 <143874680+Bersek3@users.noreply.github.com>
2024-07-27 07:33:13 +0000

    Revert "hola"
    This reverts commit e0314dad8f7cc970d11ede675a967047663cfdb5.

--- src/utils/structures/NoStackError.ts
+++
@@ -1,7 +0,0 @@
-export class NoStackError extends Error {
-    public constructor(message?: string | undefined) {
-        super(message);
-        this.stack = this.message;
-        this.name = "NoStackError";
-    }
-}
